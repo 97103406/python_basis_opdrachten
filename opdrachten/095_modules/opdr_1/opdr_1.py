dd336fcfe26c5e85dd4211c0cb55bd81a2c5a78c
thijs <9713406@st.deltion.nl>
2025-03-27 14:43:40 +0100

    095

--- opdrachten/095_modules/opdr_1/opdr_1.py
+++ opdrachten/095_modules/opdr_1/opdr_1.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # importeer de module csv...
+
+
+# csv.py
+def read_csv(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.readlines()
+            return content
+    except FileNotFoundError:
+        print(f"Het bestand {file_path} werd niet gevonden.")
+        return None
